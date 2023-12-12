73f39496594f6f84331511dd14d2b64f3518ab6e
Mees Lindeman <mees@lindeman.nu>
2023-12-12 20:00:30 +0100

    Sets heads as a variable for convenience

--- agents.py
+++ agents.py
@@ -8,10 +8,11 @@ class Sender(nn.Module):
     def __init__(self, embedding_size, hidden_size):
         super(Sender, self).__init__()
         self.num_node_features = 2
+        self.heads = 2
 
-        self.conv1 = GATv2Conv(self.num_node_features, embedding_size, edge_dim=1, heads=1, concat=True)
-        self.conv2 = GATv2Conv(-1, embedding_size, edge_dim=1, heads=1, concat=True)
-        self.fc = nn.Linear(embedding_size, hidden_size)
+        self.conv1 = GATv2Conv(self.num_node_features, embedding_size, edge_dim=1, heads=self.heads, concat=True)
+        self.conv2 = GATv2Conv(-1, embedding_size, edge_dim=1, heads=self.heads, concat=True)
+        self.fc = nn.Linear((embedding_size * self.heads), hidden_size)
 
     def forward(self, data: Data, target_node_idx) -> torch.Tensor:
         x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
@@ -37,21 +38,22 @@ class Receiver(nn.Module):
     def __init__(self, embedding_size, hidden_size):
         super(Receiver, self).__init__()
         self.num_node_features = 2
+        self.heads = 2
 
-        self.conv1 = GATv2Conv(self.num_node_features, embedding_size, edge_dim=1, heads=1, concat=True)
-        self.conv2 = GATv2Conv(-1, embedding_size, edge_dim=1, heads=1, concat=True)
-        self.fc1 = nn.Linear(hidden_size, embedding_size)
+        self.conv1 = GATv2Conv(self.num_node_features, embedding_size, edge_dim=1, heads=self.heads, concat=True)
+        self.conv2 = GATv2Conv(-1, embedding_size, edge_dim=1, heads=self.heads, concat=True)
+        self.fc = nn.Linear(hidden_size, (embedding_size * self.heads))
 
     def forward(self, message, data, _aux_input=None):
         x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
 
-        x = self.conv1(x=x, edge_index=edge_index, edge_attr=edge_attr)     # nodes x embedding_size
+        x = self.conv1(x=x, edge_index=edge_index, edge_attr=edge_attr)     # nodes x embedding size
         x = F.relu(x)
 
-        x = self.conv2(x=x, edge_index=edge_index, edge_attr=edge_attr)     # nodes x embedding_size
+        x = self.conv2(x=x, edge_index=edge_index, edge_attr=edge_attr)     # nodes x embedding size
         x = F.relu(x)   
 
-        message_embedding = self.fc1(message)                               # 1 x embeddin_size
+        message_embedding = self.fc(message)                                # 1 x embedding size
 
         dot_products = torch.matmul(x, message_embedding.t())               # nodes x 1
 
