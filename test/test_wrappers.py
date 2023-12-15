0f14d49e98994ffa9f60999e10d9c066123df0ed
Mees Lindeman <mees@lindeman.nu>
2023-12-15 17:54:12 +0100

    Commit before main

---
+++ test/test_wrappers.py
@@ -0,0 +1,50 @@
+import torch
+import egg.core as core
+import torch.nn.functional as F
+import random
+from graph.dataset import FamilyGraphDataset
+from agents import Sender, Receiver
+from options import Options
+
+def get_game(options: Options):
+    sender = Sender(options.embedding_size, options.hidden_size, temperature=1.0)
+    receiver = Receiver(options.embedding_size, options.hidden_size)
+
+    if options.training_mode == 'gs':
+        sender_wrapped = core.RnnSenderGS(sender, options.vocab_size, options.embedding_size, options.hidden_size, max_len=5, temperature=1.0, cell=options.sender_cell)
+        receiver_wrapped = core.RnnReceiverGS(receiver, options.vocab_size, options.embedding_size, options.hidden_size, cell=options.sender_cell)
+        game = core.SenderReceiverRnnGS(sender_wrapped, receiver_wrapped, loss)
+    elif options.training_mode == 'rf':
+        sender_wrapped = core.RnnSenderReinforce(sender, options.vocab_size, options.embedding_size, options.hidden_size, max_len=5)
+        receiver_wrapped = core.RnnReceiverReinforce(receiver, options.vocab_size, options.embedding_size, options.hidden_size)
+        game = core.SenderReceiverRnnReinforce(sender_wrapped, receiver_wrapped, loss)
+    else:
+        raise ValueError(f"Unknown training mode: {options.training_mode}")
+
+    return game
+
+def loss(_sender_input, _message, _receiver_input, receiver_output, labels, _aux_input):
+        nll = F.nll_loss(receiver_output, labels, reduction="none")
+        acc = (labels == receiver_output.argmax(dim=1)).float().mean()
+        return nll, {"acc": acc}
+
+# Test script
+if __name__ == "__main__":
+    options = Options()
+
+    # Load the dataset and extract a graph
+    dataset = FamilyGraphDataset(root='/Users/meeslindeman/Library/Mobile Documents/com~apple~CloudDocs/Thesis/Code/data', number_of_graphs=10, generations=3)
+    data = dataset[0]
+
+    # Initialize the game
+    game = get_game(options)
+
+    # Sender produces a message
+    sender_output = game.sender(data)
+    print("Sender's message:", sender_output)
+    print("Sender's shape:", sender_output.shape)
+
+    # Receiver tries to identify the target node
+    receiver_output = game.receiver(sender_output, data)
+    print("Receiver's output:", receiver_output)
+    print("Receiver's shape:", receiver_output.shape)
